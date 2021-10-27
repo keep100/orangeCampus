47243eae948c77db3312fad468bfe185d345fee9
keep100 <78190076+keep100@users.noreply.github.com>
2021-10-27 15:47:28 +0800

    Add files via upload

---
+++ src/cn/orangecampus/utils/JdbcTools.java
@@ -0,0 +1,45 @@
+package cn.orangecampus.utils;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author 16321
+ */
+public class JdbcTools {
+    private static ComboPooledDataSource dataSource;
+
+    static {
+        dataSource = new ComboPooledDataSource();
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void release(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
